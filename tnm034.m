f35a77fd264e52a784c01879f386ae3c6aae7fac
Jennifer Bedhammar <jennifer95@hotmail.com>
2018-11-09 10:29:02 +0100

    Setup

---
+++ tnm034.m
@@ -0,0 +1,17 @@
+function [ strout ] = tnm034( im )
+% Authors: Emma Weberyd, Jennifer Bedhammar, Julius Kördel, Oliver
+% Johansson.
+% Last edit: 2018-11-09
+
+% Function information:
+% Im: Input image of captured sheet music. Im should be in double format,
+% normalized to the interval [0,1]
+% Strout: The resulting character string of the detected notes. The string
+% must follow a pre-defined format. Pitch G2-E4. 
+% Quarter notes (crotchets) uppercase letters. 
+% Eight notes (quavers) lowercase letters.
+
+
+
+end
+
