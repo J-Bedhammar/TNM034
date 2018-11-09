49c384bfec390e47e782f01cd9de4318dd2ea2af
Sparvolle <Oliverj96@hotmail.com>
2018-11-09 11:40:09 +0100

    BinaryShift

--- main.m
+++ main.m
@@ -1,8 +1,10 @@
 %% TNM034 OMR Project
 
+clear all
+clc
 % Load testimage
 path = fullfile('Images_Training', 'Le_1_Example.jpg');
 testImage = imread(path);
 
 %% Call function
-output = tnm034( image );
+output = tnm034( testImage );
