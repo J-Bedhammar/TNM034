23ec08d2e4c656cc1a0a64ac1ceab082d258c271
Sparvolle <Oliverj96@hotmail.com>
2018-11-12 10:20:10 +0100

    Morph update, good result

--- MorphOperation.m
+++ MorphOperation.m
@@ -1,8 +1,8 @@
 function [outim] = MorphOperation(input)
 % Morphs the image to thin out the thickest lines and remove the rest.
-
-
-outim = bwmorph(input, 'fill');
+out1 = bwmorph(input, 'dilate');
+out2 = bwmorph(out1, 'bridge');
+outim = bwmorph(out2, 'erode');
 
 % outim = bwmorph(input, 'close');
 end
