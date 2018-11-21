65c39f1eead35af955be2e7b3b94c74bcb131998
Jennifer Bedhammar <jennifer95@hotmail.com>
2018-11-21 15:37:18 +0100

    PitchLines - matrix with every pitch

---
+++ PitchLines.m
@@ -0,0 +1,30 @@
+function [ output ] = PitchLines( inputLines, dist )
+% Author: Jennifer Bedhammar
+% Last edit: 2018-11-21
+
+% Check that input lines consists of 5 lines?
+if (mod(length(inputLines), 5 ~= 0 ))
+    error('Wrong number of lines')
+end
+
+% Variables
+nrPitches = 20;                          % G1 - E4
+rows = length(inputLines) / 5;           % Number of rows
+pitchLines = zeros(rows,nrOfPitches);
+firstLine = 1;
+
+% Save y-values of every pitch in matrix
+for i = 1:rows
+    nextPitch = 0;
+    for j = 1:nrPitches
+       pitchLines(i,j) = inputLines(firstLine) - (6.5*d) + nextPitch;
+       nextPitch = nextPitch + (dist/2);
+    end
+    firstLine = firstLine + 5;
+end
+
+% return matrix 
+output = pitchLines;
+
+end
+
