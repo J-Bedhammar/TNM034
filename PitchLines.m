1b9743824778c69c584fcfabd70c7d4636928369
Jennifer Bedhammar <jennifer95@hotmail.com>
2018-11-21 16:05:15 +0100

    Update PitchLines, wrong order
    OBS! dist only based on the first two lines

--- PitchLines.m
+++ PitchLines.m
@@ -1,4 +1,4 @@
-function [ output ] = PitchLines( inputLines, dist )
+function [ output ] = PitchLines( inputLines )
 % Author: Jennifer Bedhammar
 % Last edit: 2018-11-21
 
@@ -10,14 +10,15 @@ end
 % Variables
 nrPitches = 20;                          % G1 - E4
 rows = length(inputLines) / 5;           % Number of rows
-pitchLines = zeros(rows,nrOfPitches);
+pitchLines = zeros(rows,nrPitches);
 firstLine = 1;
+dist = inputLines(2)-inputLines(1);      % OBS! dist based on two lines only
 
 % Save y-values of every pitch in matrix
 for i = 1:rows
     nextPitch = 0;
     for j = 1:nrPitches
-       pitchLines(i,j) = inputLines(firstLine) - (6.5*d) + nextPitch;
+       pitchLines(i,j) = inputLines(firstLine) + (6.5*dist) - nextPitch;
        nextPitch = nextPitch + (dist/2);
     end
     firstLine = firstLine + 5;
