fc15e45c1020cc38662f3aff79da9fbbf07c6c41
Sparvolle <Oliverj96@hotmail.com>
2018-11-22 09:01:16 +0100

    Getpitch and LineDistance updated

--- GetPitch.m
+++ GetPitch.m
@@ -1,4 +1,4 @@
-function [pitch] = GetPitch(noteheads, pitchlines)
+function [out] = GetPitch(noteheads, pitchlines)
 % Author: Oliver Johansson
 
 % Function information:
@@ -9,11 +9,12 @@ function [pitch] = GetPitch(noteheads, pitchlines)
 Centroid = regionprops(noteheads, 'Centroid');
 
 % get the closest stemrow
-row = 10000;
+neardist = 10000;
 for i = 1 : length(pitchlines(:,10))
     temp = abs(Centroid{2} - pitchlines(i,10));
-    if temp < row
-        row = temp;
+    if temp < neardist
+        row = i;
+        neardist = temp;
     end
 end
 
@@ -22,9 +23,18 @@ nearest = 10000;
 for j = 1 : length(pitchlines(row,i))
     temp = abs(Centroid{2} - pitchlines(row,i));
     if temp < nearest
-        pitch = pitchlines(row,i);
+        pitch = i;
+        nearest = temp;
     end
 end
+
+pitchstring = ["G1","A1","B1", "C2", "D2", "E2", "F2",
+               "G2","A2","B2", "C3", "D3", "E3", "F3", 
+               "G3","A3","B3", "C4", "D4", "E4"];
+
+
+out = pitchstring(pitch);
+
 
 end
 
