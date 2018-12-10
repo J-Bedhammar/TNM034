e732c7b6366499f4c96b8b86423624bdd3f88c5a
Julius Kördel <kordel.julius@gmail.com>
2018-12-10 10:59:51 +0100

    inte klar

--- notetype.m
+++ notetype.m
@@ -1,21 +1,21 @@
-function [rensadnote2] = notetype(rensadnote,labeledImg,labels,template)
+function [rensadnote2] = notetype(rensadnote,labeledImg,labels,template,dist)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 bw  = double(255 * labeledImg);
 RGB = cat(3, bw, bw, bw);
-
 for i = 1:length(rensadnote)
  %[ri,ci] = find(rensadnote{1,i} == noteArray{})
         [r, c] = find(rensadnote{1,i});
-        [ri, ci] = find(labeledImg == labels(3));
+        [ri, ci] = find(labeledImg == labels(i));
+        
 %         RGB(r,c,1) =255;
 %         RGB(r,c,2) =0;
 %         RGB(r,c,3) =0;
-%         l = floor(length(template(1,:))/4);
-         rt1  =(min(r)):(max(r));
-         ct1 = (min(c)):(max(c));
+         l = floor(length(template(1,:))/4);
+         rt1  =(min(ri)-l):(max(ri)+l);
+         ct1 = (min(ci)-l):(max(ci)+l);
         if(length(rt1) > 3 && length(ct1)>3)
-            noteclass = LocalProj(rensadnote{1,i},template,0);
+            noteclass = LocalProj(rensadnote{1,i},template,1,dist);
             rensadnote{2,i} = noteclass;
         end
     
